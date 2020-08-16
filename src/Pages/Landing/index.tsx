0b650250fcc454ac6365e340e8e4c7952cb9092f
Xavi002 <diogomartins200214@gmail.com>
2020-08-16 13:08:27 +0100

    first commit

---
+++ src/Pages/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, TextContainer, Title, Slogan } from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <Container>
+      <TextContainer>
+        <Title>Hi</Title>
+        <Slogan></Slogan>
+      </TextContainer>
+    </Container>
+  );
+}
+
+export default Landing;
---
+++ src/Pages/Landing/styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  
+`;
+
+export const TextContainer = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Slogan = styled.Text``;
